1cd77df4b833056fb44b7360a64e0bbbf9962b21
anujkumar220103 <24204031218@stu.manit.ac.in>
2025-07-11 15:36:10 +0530

    new commit

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ app.use(express.json())
 app.use(clerkMiddleware())
 
 //API to listen clerk Webhooks
-app.use("/api/clerk", clerkWebhooks);
+app.post("/api/clerk", express.raw({ type: "application/json" }), clerkWebhooks);
 
 app.get('/', (req, res)=> res.send("API is working fine"))
 
