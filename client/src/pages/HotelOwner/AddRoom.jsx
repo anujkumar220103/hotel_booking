1fe39874c7fc1577b49d3be12f825c0a82cecba9
anujkumar220103 <24204031218@stu.manit.ac.in>
2025-07-19 14:50:08 +0530

    project completed

--- client/src/pages/HotelOwner/AddRoom.jsx
+++ client/src/pages/HotelOwner/AddRoom.jsx
@@ -1,9 +1,13 @@
 import React, { useState } from "react";
 import Title from "../../components/Title";
 import { assets } from "../../assets/assets";
+import { useAppContext } from "../../context/AppContext";
+import toast from "react-hot-toast";
 
 
 const AddRoom= () => {
+
+    const {axios, getToken} = useAppContext() 
 
     const [images, setImages] = useState({
         1: null,
@@ -24,8 +28,57 @@ const AddRoom= () => {
         }
     })
 
+    const [loading, setLoading] = useState(false)
+
+    const onSubmitHandler = async (e)=>{
+        e.preventDefault()
+        //check if all input are filled
+        if(!inputs.roomType || !inputs.pricePerNight || !inputs.amenities || !Object.values(images).some(image => image)){
+            toast.error("Please fill in all the details")
+            return;
+        }
+        setLoading(true);
+        try {
+            const formData = new FormData()
+            formData.append('roomType', inputs.roomType)
+            formData.append('pricePerNight', inputs.pricePerNight)
+            //converting amenities to array & keeping only enabled amenities
+            const amenities = Object.keys(inputs.amenities).filter(key => inputs.amenities[key])
+            formData.append('amenities', JSON.stringify(amenities))
+
+            //Adding Images to FormData
+            Object.keys(images).forEach((key)=>{
+                images[key] && formData.append('images', images[key])
+            })
+            
+            const { data } = await axios.post('/api/rooms/', formData, {headers: {Authorization: `Bearer ${await getToken()}`}})  //555555555555555555555555555555555555
+
+            if(data.success){
+                toast.success(data.message)
+                setInputs({
+                    roomType: '',
+                    pricePerNight: 0,
+                    amenities: {
+                        'Free Wifi': false,
+                        'Free Breakfast': false,
+                        'Room Service': false,
+                        'Mountain View': false,
+                        'Pool Access': false
+                    }
+                })
+                setImages({1: null, 2:null, 3: null, 4:null})
+            }else{
+                toast.error(data.message);
+            }
+        } catch (error) {
+            toast.error(error.message);
+        }finally{
+            setLoading(false);
+        }
+    }
+
     return (
-        <form>
+        <form onSubmit={onSubmitHandler}>
             <Title align='left' font='outfit' title='Add Room' subTitle='fill in the details carefully and accurate room details, pricing, and amenities, to enhance the user booking experience'/>
             {/* Uplaod Area For Images */}
             <p className="text-gray-800 mt-10">Images</p>
@@ -42,7 +95,7 @@ const AddRoom= () => {
             <div className='w-full flex max-sm:flex-col sm:gap-4 mt-4'>
                 <div className='flex--1 max-w-48'>
                     <p className='text-gray-800 mt-4'>Room Type</p>
-                    <select value={inputs.roomType} onChange={e=>setInput({...inputs, roomType: e.target.value})}
+                    <select value={inputs.roomType} onChange={e=>setInputs({...inputs, roomType: e.target.value})}
                     className='border opacity-70 border-gray-300 mt-1 rounded p-2 w-full'>
                         <option value="">Select Room Type</option>
                         <option value="Single Bed">Single Bed</option>
@@ -69,8 +122,8 @@ const AddRoom= () => {
                     </div>
                 ))}
             </div>
-            <button className='bg-primary text-white px-8 py-2 rounded mt-8 cursor-pointer'>
-                Add Room
+            <button className='bg-primary text-white px-8 py-2 rounded mt-8 cursor-pointer' disabled={loading}>
+                {loading ? 'Adding...' : "Add Room"}
             </button>
         </form>
     )
